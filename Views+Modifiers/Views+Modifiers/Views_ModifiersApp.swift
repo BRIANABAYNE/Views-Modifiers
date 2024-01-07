a18e108881dcc6e0c516dc2b877c764952e38c76
Briana Bayne <brianabayne1@gmail.com>
2024-01-07 11:51:11 -0700

    Initial Commit

---
+++ Views+Modifiers/Views+Modifiers/Views_ModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Views_ModifiersApp.swift
+//  Views+Modifiers
+//
+//  Created by Briana Bayne on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Views_ModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
